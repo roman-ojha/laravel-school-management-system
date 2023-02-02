3ad3f4f6585444f2ffa9a4abc3453bde682829c1
Roman-Ojha <razzroman98@gmail.com>
2023-02-02 17:55:04 +0545

    feat(migration): created teacher_subject m-t-m table

---
+++ database/migrations/2023_02_02_120654_create_teacher_subject_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacher_subject', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('teacher_id')->constrained('teacher');
+            $table->foreignId('subject_id')->constrained('subject');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacher_subject');
+    }
+};
