0fb1cdb4ddb3f124d22a43e030b39d471c83ad8f
Roman-Ojha <razzroman98@gmail.com>
2023-02-02 20:52:40 +0545

    feat: added css assets inside project

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.global')
 
+@section('css')
+    @vite('resources/css/admin/index.css')
+@endsection
+
 @section('title', 'admin')
 
 @section('main')
--- resources/views/layout/global.blade.php
+++ resources/views/layout/global.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link rel="stylesheet" href="style.css"> --}}
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href={{ asset('style.css') }} />
+    @yield('css')
     <title>@yield('title')</title>
 </head>
 
