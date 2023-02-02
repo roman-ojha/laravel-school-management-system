8c66045d73cc0c86e555f050e8f9a77082c2ecd6
Roman-Ojha <razzroman98@gmail.com>
2023-02-02 18:03:27 +0545

    feat(migration): created library_student & library_teacher table

---
+++ database/migrations/2023_02_02_121533_create_library_student_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('library_student', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained('student');
+            $table->foreignId('library_id')->constrained('library');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('library_student');
+    }
+};
---
+++ database/migrations/2023_02_02_121715_create_library_teacher_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('library_teacher', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('teacher_id')->constrained('teacher');
+            $table->foreignId('library_id')->constrained('library');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('library_teacher');
+    }
+};
