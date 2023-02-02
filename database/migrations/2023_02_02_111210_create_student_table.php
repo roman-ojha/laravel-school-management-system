befd5f1c31b10aba662bef99fcfe9134dbeccf26
Roman-Ojha <razzroman98@gmail.com>
2023-02-02 16:57:52 +0545

    feat: created student table migration

---
+++ database/migrations/2023_02_02_111210_create_student_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->char("name", 100);
+            $table->integer("roll");
+            $table->date("batch");
+            $table->char("faculty", 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+};
