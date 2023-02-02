0d057ff7e60b84cd3bed60bcb1e696e9509e9dfb
Roman-Ojha <razzroman98@gmail.com>
2023-02-02 17:31:02 +0545

    feat: created book table migration

---
+++ database/migrations/2023_02_02_113059_create_book_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->char("name", 50);
+            $table->char("publication", 70);
+            $table->date("release-on");
+            $table->integer("page");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+};
